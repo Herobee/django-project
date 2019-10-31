bea2a359f33bde89cc82357ef5b5132d0f1d16ce
herobee <jswo0315@gmail.com>
2019-10-31 15:05:37 +0900

    Finished Learn django following YouTube..

--- trydjango/courses/views.py
+++ trydjango/courses/views.py
@@ -1,15 +1,116 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
+
+from .forms import CourseModelForm
+from .models import Course
 
 # BASE VIEW Class = VIEW
 
-class CourseView(View):
-    def get(self, reqeust, *args, **kwargs):
-        return render(request, 'about.html', {})
+class CourseObjectMixin(object):
+    model = Course
+    lookup = 'id'
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+        return obj
+
+class CourseDeleteView(CourseObjectMixin, View):
+    template_name = "courses/course_delete.html" # DetailView
+
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            context['object'] = obj
+        return render(request, self.template_name, context)
+        
+    def post(self, request, id=None, *args, **kwargs):
+        # POST method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            obj.delete()
+            context['object'] = None
+            return redirect('/courses/')
+        return render(request, self.template_name, context)
+
+class CourseUpdateView(CourseObjectMixin, View):
+    template_name = "courses/course_update.html" # DetailView
+    def get_object(self):
+        id = self.kwargs.get('id')
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+        return obj
+
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = CourseModelForm(instance=obj)
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+        
+    def post(self, request, id=None, *args, **kwargs):
+        # POST method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = CourseModelForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+
+class CourseCreateView(View):
+    template_name = "courses/course_create.html" # DetailView
+    def get(self, request, *args, **kwargs):
+        # GET method
+        form = CourseModelForm()
+        context = {"form" : form}
+        return render(request, self.template_name, context)
+    def post(self, request, *args, **kwargs):
+        # POST method
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = CourseModelForm()
+        context = {"form" : form}
+        return render(request, self.template_name, context)
+
+
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context =  {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
+
+# class MyListView(CourseListView):
+#     queryset = Course.objects.filter(id=1)
+
+class CourseView(CourseObjectMixin, View):
+    template_name = "courses/course_detail.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {'object': self.get_object()}
+        return render(request, self.template_name, context)
 
     # def post(reqeust, *args, **kwargs):
     #     return render(request, 'about.html', {})
+    
 # HTTP METHODS
-def my_fbv(reqeust, *args, **kwargs):
+def my_fbv(request, *args, **kwargs):
     print(request.method)
     return render(request, 'about.html', {})
